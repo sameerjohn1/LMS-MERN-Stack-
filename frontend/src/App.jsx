bca6d2a01b7b7e2bad4ad9537bbca8733b03fa75
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-02 13:37:30 +0500

    Add Courses page UI and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Faculty from "./pages/Faculty";
+import Courses from "./pages/Courses";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/faculty" element={<Faculty />} />
+        <Route path="/courses" element={<Courses />} />
       </Routes>
     </>
   );
