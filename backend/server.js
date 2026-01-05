7735c5e45e55409b183c4798cec6f453e75e93ed
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-01-05 17:09:42 +0500

    chore(structure): add backend project folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 const port = 4000;
@@ -9,6 +10,8 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(clerkMiddleware());
 
 // routes
 
